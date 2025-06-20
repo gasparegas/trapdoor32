17986fd266f3461731f5a346aa925fadb203d90c
gas <gasparecartuccio@hotmail.com>
2025-06-21 00:12:23 +0200

    Fix display

---
+++ lib/TFT_eSPI/User_Setup.h
@@ -0,0 +1,29 @@
+#define USER_SETUP_ID 123
+
+#define ST7789_DRIVER
+
+#define TFT_WIDTH 135
+#define TFT_HEIGHT 240
+
+#define CGRAM_OFFSET
+
+#define TFT_MISO -1
+#define TFT_MOSI 19
+#define TFT_SCLK 18
+#define TFT_CS   5
+#define TFT_DC   16
+#define TFT_RST  23
+#define TFT_BL   4
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+#define SMOOTH_FONT
+
+#define SPI_FREQUENCY  27000000
+#define SPI_READ_FREQUENCY  16000000
+#define SPI_TOUCH_FREQUENCY  2500000
--- src/icons.hpp
+++
@@ -1,3 +0,0 @@
-// Placeholder for SVG icons
-const char google_icon[] PROGMEM = "<svg><!-- Google icon SVG --></svg>";
-const char facebook_icon[] PROGMEM = "<svg><!-- Facebook icon SVG --></svg>";
